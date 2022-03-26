7725ca6b952bb6352cd020d8d152edae1206d9de
Dana <markova.dana@email.cz>
2022-03-26 19:36:52 +0100

    tlacitko

--- sportka.js
+++ sportka.js
@@ -1,24 +1,35 @@
 // SPORTKA
 
-let osudi = [];
-let tazenaCisla = [];
+
 let vyherniCisla = document.querySelector('#vyherni-cisla');
 
-// generovani osudi
-for (let i = 1; i <= 49; i = i + 1) {
-    osudi.push(i);
-}
 
-// z osudi vyberu 7 nahodnych cisel
-for (let i = 0; i < 7; i++) {
-    let vyherniIndex = Math.floor(Math.random() * osudi.length);
-    let vyherniCislo = osudi[vyherniIndex];
-    
-    tazenaCisla.push(vyherniCislo);
-    osudi.splice(vyherniIndex, 1);
-}
+//tlacitko start
+function start() {
+    let osudi = [];
+    let tazenaCisla = [];
+    vyherniCisla.innerHTML = ""; // zajistí, že se předchozí tažená čísla vymažou
 
-// vypis do HTML
-for (let j = 0; j < tazenaCisla.length; j++) {
-    vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[j] + '</span>';
+    document.querySelector('#losuj');
+    // generovani osudi
+    for (let i = 1; i <= 49; i = i + 1) {
+        osudi.push(i);
+    }
+
+    // z osudi vyberu 7 nahodnych cisel
+    for (let i = 0; i < 7; i++) {
+        let vyherniIndex = Math.floor(Math.random() * osudi.length);
+        let vyherniCislo = osudi[vyherniIndex];
+
+        tazenaCisla.push(vyherniCislo);
+        osudi.splice(vyherniIndex, 1);
+    }
+
+    // vypis do HTML
+    for (let j = 0; j < tazenaCisla.length; j++) {
+        setInterval(function () {
+            vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[j] + '</span>';
+        }, 1000)
+
+    }
 }
